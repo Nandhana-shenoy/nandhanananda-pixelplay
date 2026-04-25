4c7386817e6f3da1703da7ed1fa2fe33b0e774b0
Nandhanananda Shenoy <nandhanashenoy45@gmail.com>
2026-04-25 14:50:26 +0530

    Update BrowserRouter basename for routing

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => (
   <TooltipProvider>
     <Toaster />
     <Sonner />
-    <BrowserRouter>
+<BrowserRouter basename="/nandhanananda-pixelplay/">
       <Routes>
         <Route path="/" element={<Index />} />
         {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
